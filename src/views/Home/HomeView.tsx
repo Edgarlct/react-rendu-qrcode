1ecf545dd13a6b8f5c61f710653a42feb2e66671
Edouard Nicolas <edouard.n@lifeaz.fr>
2023-02-03 16:31:07 +0100

    adding style to home page

--- src/views/Home/HomeView.tsx
+++ src/views/Home/HomeView.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import {Typography} from "@mui/material";
+import "./HomeView.scss";
+
 
 export default function HomeView() {
       return (
-          <div>
-            <Typography variant={"h1"}>HomeView</Typography>
+          <div className={"homeContainer"}>
+            <div className={"leftSideHome"}>
+              <Typography variant={"h3"} color={"secondary"}>Générez vos qr code en toute simplicité</Typography>
+              <Typography variant={"h6"} color={"primary.dark"}>
+                Nous vous offrons la possibilité de générer vos qr code et
+                de consultée des statistique de vos qr code tout en respectant la vie privé
+              </Typography>
+            </div>
+            <div className={"rightSideHome"}>
+              <img src={"/stat.png"} alt={"statistique"} className={"statImage"}/>
+            </div>
           </div>
       );
 }
